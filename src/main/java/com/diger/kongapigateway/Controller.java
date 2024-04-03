3b75edbf771470af675aeeef0c972ca1db5e0438
Diger <kitten.diger@gmail.com>
2024-04-03 20:35:23 +0900

    Controller, DockerFile, Docker-ComposeFile

---
+++ src/main/java/com/diger/kongapigateway/Controller.java
@@ -0,0 +1,41 @@
+package com.diger.kongapigateway;
+
+import java.time.LocalDateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class Controller {
+
+    private static final Logger log = LoggerFactory.getLogger(Controller.class);
+
+    @GetMapping("/api/v1/users")
+    public String getUsersMock() {
+        log.info(LocalDateTime.now().toString());
+        return "유저마이크로서비스 GET";
+    }
+
+    @GetMapping("/users")
+    public String getUsers() {
+        log.info(LocalDateTime.now().toString());
+        return "유저마이크로서비스 GET";
+    }
+
+    @PostMapping("/users")
+    public String postUsers() {
+        return "유저마이크로서비스 POST";
+    }
+
+    @GetMapping("/products")
+    public String getProducts() {
+        return "상품마이크로서비스 GET";
+    }
+
+    @PostMapping("/products")
+    public String postProducts() {
+        return "상품마이크로서비스 POST";
+    }
+}
---
+++ src/main/java/com/diger/kongapigateway/KongApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.diger.kongapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KongApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KongApiGatewayApplication.class, args);
+    }
+
+}
